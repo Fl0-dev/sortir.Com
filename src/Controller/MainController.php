1e64084fde099a0435640ef9e56e2b60292dd033
Fl0-dev <florian.girard.dev@outlook.fr>
2021-08-17 15:05:35 +0200

    fix RechercheSortieType

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\RechercheSortieType;
 use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,9 +24,39 @@ class MainController extends AbstractController
      */
     public function list(SortieRepository $sortieRepository):Response
     {
+        //liste des sorties sans recherche
         $sorties = $sortieRepository->findBy([],["dateHeureDebut"=>"DESC"]);
+        //mise en route du du formulaire de recherche
+        $sortieForm = $this->createForm(RechercheSortieType::class);
+
         return $this->render('main/accueil.html.twig',[
             "sorties"=>$sorties,
+            "sortieForm"=>$sortieForm->createView(),
         ]);
     }
+
+    /**
+     * @Route("/accueil/recherche", name="recherche")
+     */
+    public function recherche(SortieRepository $sortieRepository, Request $request):Response
+    {
+        //liste des sorties sans recherche
+        $sorties = $sortieRepository->findBy([],["dateHeureDebut"=>"DESC"]);
+        //mise en route du du formulaire de recherche
+        $sortieForm = $this->createForm(RechercheSortieType::class);
+        // retour de la réponse
+        $sortieForm->handleRequest($request);
+        //si form soumis et valide
+        if ($sortieForm->isSubmitted() && $sortieForm->isValid()){
+            //TODO:hydratation pour recherche
+            //TODO:recherches persos
+            //TODO:return la recherche
+            return $this->redirectToRoute('accueil',[],Response::HTTP_SEE_OTHER);
+        }
+        return $this->render('main/accueil.html.twig',[
+            "sorties"=>$sorties,
+            "sortieForm"=>$sortieForm,
+        ]);
+    }
+
 }
