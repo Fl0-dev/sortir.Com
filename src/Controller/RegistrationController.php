1af54887455f6ccb9a4552bd43afb7bc381ba350
Fl0-dev <florian.girard.dev@outlook.fr>
2021-08-17 09:57:48 +0200

    Fix du RegisterForm

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -18,7 +18,7 @@ class RegistrationController extends AbstractController
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $user = new User();
-        $user->setRoles(["ROLE_USER"]);
+        $user->setRoles(["ROLE_USER"]);//TODO: choix des roles ?
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
