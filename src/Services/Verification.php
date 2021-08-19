c51954e407685d445a8b9716b7e66b5728ae70d0
Fl0-dev <florian.girard.dev@outlook.fr>
2021-08-19 17:29:03 +0200

    ajout contrainte dans l'entity RechercheSortie.php

--- src/Services/Verification.php
+++ src/Services/Verification.php
@@ -2,9 +2,9 @@
 
 namespace App\Services;
 
-use App\Entity\Sortie;
-use App\Entity\User;
+use App\Repository\EtatRepository;
 use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class Verification
 {
@@ -30,21 +30,46 @@ class Verification
     /**
      * verifie l'état des sortie par rapport aux dates et le change si besoin
      * @param SortieRepository $sortieRepository
+     * @param EntityManagerInterface $entityManager
      */
-    public function verifEtatSortie(SortieRepository $sortieRepository){
+    public function verifEtatSortie(SortieRepository $sortieRepository,EntityManagerInterface $entityManager,
+            EtatRepository $etatRepository){
 
-        //si etat ouvert ou cloturé :
+        //date du jour
+        $today = new \DateTime();
 
-        //vérification par rapport à aujourd'hui
+        //récupération de toutes les sorties
+        $sorties = $sortieRepository->findAll();
+        //récupération des états voulus
+        $etatCloture = $etatRepository->find(3);
 
-        //si date = aujourd'hui
+        //pour chaque sortie
+        foreach ($sorties as $sortie){
+            //si état ouvert
+            if ($sortie->getEtat()->getId()==2) {
+                //si date d'insciption > date d'aujourd'hui
+                if($sortie->getDateLimiteInscription()>$today){
+                    $sortie->setEtat($etatCloture);
+                }
+                //si date aujourdhui > activité
+                if($sortie->getDateHeureDebut()>$today){
+                    //si diff entre dateDuDebut et today inf à 0
+                    $interval = date_diff($sortie->getDateHeureDebut() + $sortie->getDuree(), $today);
+                }
 
-            //si heure >heure de début
+
+                //vérification par rapport à aujourd'hui
+
+                //si date = aujourd'hui
+
+                //si heure >heure de début
                 //calcul du temps avant fin d'activité
 
                 // si inférieure ->état débuté
 
-
+                // si supérieure ->état passé
+            }
+        }
 
 
     }
