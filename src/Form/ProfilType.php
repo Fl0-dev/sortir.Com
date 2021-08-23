413bc710606e1a4378fc76770052c9eef734f148
Fl0-dev <florian.girard.dev@outlook.fr>
2021-08-23 12:01:01 +0200

    Mise en place de la photo de profil =>TODO : affichage et correction du champ dans le formulaire

--- src/Form/ProfilType.php
+++ src/Form/ProfilType.php
@@ -4,10 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class ProfilType extends AbstractType
 {
@@ -20,17 +22,27 @@ class ProfilType extends AbstractType
             ->add('telephone')
             ->add('email')
             ->add('new_password', RepeatedType::class, [
+                'required'=>false,
                 'type' => PasswordType::class,
                 "mapped"=>false,
                 'invalid_message' => 'Les mots de passe doivent correspondre.',
                 'options' => ['attr' => ['class' => 'password-field']],
-                //'required' => true,
                 'first_options'  => ['label' => 'Nouveau mot de passe'],
                 'second_options' => ['label' => 'Confirmation'],
             ])
             ->add('plainPassword', PasswordType::class,["mapped"=>false, 'label' => 'Mot de passe'])
             ->add('campus', null, ["choice_label"=>"nom"])
-            //->add('photo')
+            ->add('photo',FileType::class,[
+                'mapped'=>false,
+                'required'=>false,
+                'label'=>'Ma photo (format .jpg)',
+                'constraints'=>[
+                    new File([
+                       'maxSize'=>'5000k',
+                        'mimeTypesMessage'=>'On veux une photo en .jpg'
+                    ])
+                ]
+            ])
             //->add('roles')
         ;
     }
