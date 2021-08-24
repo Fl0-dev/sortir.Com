609fa6dfe1aff599cda072301e6ed71796498517
Fl0-dev <florian.girard.dev@outlook.fr>
2021-08-24 15:38:33 +0200

    Ajout volet admin avec lien

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,10 +15,25 @@ class AdminController extends AbstractController
     /**
      * @Route("accueil/", name="accueil")
      */
-    public function index(): Response
+    public function accueilAdmin(): Response
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+        //récupération de l'user connecté
+        $user = $this->getUser();
+        return $this->render('admin/accueilAdmin.html.twig',[
+            'user'=>$user,
+        ]);
+    }
+
+    /**
+     * @Route("accueil/gestionUsers", name="gestionUsers")
+     */
+    public function gestionUsers(UserRepository $userRepository): Response
+    {
+        //récupération de la listes des users
+        $users = $userRepository->findAll();
+
+        return $this->render('admin/gestionUsers.html.twig', [
+            'users'=>$users,
         ]);
     }
 }
